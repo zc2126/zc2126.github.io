d7da11c55abae3c958caee6e03be3264f4ba7f77
zc2126 <zc2126@nyu.edu>
2023-11-07 23:21:55 -0500

    created base files

---
+++ front-end/src/Components.jsx
@@ -0,0 +1,33 @@
+// Import dependencies
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+
+// Import CSS
+import './Components.css'
+
+const ExperienceTile = props => {
+    return (
+        <div ClassName = "EXP-TILE">
+            <p>Test</p>
+        </div>
+    )
+}
+
+const ProjectTile = props => {
+    return (
+        <div ClassName = "EXP-TILE">
+            <p>Test</p>
+        </div>
+    )
+}
+
+const SkillTile = props => {
+    return (
+        <div ClassName = "EXP-TILE">
+            <p>Test</p>
+        </div>
+    )
+}
+
+export default { ExperienceTile, ProjectTile, SkillTile }
